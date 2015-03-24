a136e3ba391dc11a7ef7052afafd26d050d7b391
Ken Youens-Clark <kyclark@gmail.com>
2015-03-24 11:19:08 -0700

    smaller FA split size, renamed "create_dir" to "init_dir" to better convey meaning

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,12 +4,12 @@ export FASTA_DIR="/rsgrps/bhurwitz/hurwitzlab/data/clean/pov"
 export SPLIT_FA_DIR="/rsgrps/bhurwitz/ajacob/blast-pipeline/fasta-split"
 export BLAST_OUT_DIR="/rsgrps/bhurwitz/ajacob/blast-pipeline/blast-out"
 export SCRIPT_DIR="$CWD/workers"
-export FA_SPLIT_FILE_SIZE=5000000 # in KB
+export FA_SPLIT_FILE_SIZE=500000 # in KB
 export BLAST_CONF_FILE="/rsgrps/bhurwitz/ajacob/blast-pipeline/blastdbs"
 export BLAST="/rsgrps/bhurwitz/hurwitzlab/bin/blastall"
 export JOBS=100
 
-function create_dir {
+function init_dir {
     for dir in $*; do
         if [ -d "$dir" ]; then
             rm -rf $dir/*
