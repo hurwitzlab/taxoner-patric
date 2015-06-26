92d498bea5d62261ad536579654a08e3d278885d
Scott G. Daniel <scottdaniel25@gmail.com>
2015-06-26 11:56:57 -0700

    getting some config values from bacterial branch

--- scripts/00-split-fa.sh
+++ scripts/00-split-fa.sh
@@ -4,9 +4,9 @@ PROG=`basename $0 ".sh"`
 STDERR_DIR="$CWD/err/$PROG"
 STDOUT_DIR="$CWD/out/$PROG"
 
-init_dir "$STDERR_DIR" "$STDOUT_DIR" "$SPLIT_FA_DIR" 
+init_dir "$STDERR_DIR" "$STDOUT_DIR" "$SPLIT_FA_DIR"
 
-cd "$FASTA_DIR" 
+cd "$FASTA_DIR"
 
 export FILES_LIST="$FASTA_DIR/files-list"
 
@@ -20,5 +20,5 @@ if [ $NUM_FILES -gt 0 ]; then
     JOB_ID=`qsub -v CWD,BIN_DIR,FASTA_DIR,SPLIT_FA_DIR,FILES_LIST,FA_SPLIT_FILE_SIZE -N split-fa -e "$STDERR_DIR" -o "$STDOUT_DIR" $SCRIPT_DIR/split_fa.sh`
     echo Submitted job \"$JOB_ID.\"  Adios.
 else
-    echo Nothing to doNothing to do.
+    echo Nothing to do.
 fi
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,11 @@
 export CWD=$PWD
 export BIN_DIR="/rsgrps/bhurwitz/hurwitzlab/bin"
-export FASTA_DIR="/rsgrps/bhurwitz/hurwitzlab/data/clean/pov"
-export SPLIT_FA_DIR="/rsgrps/bhurwitz/ajacob/blast-pipeline/fasta-split"
-export BLAST_OUT_DIR="/rsgrps/bhurwitz/ajacob/blast-pipeline/blast-out"
+export FASTA_DIR="/rsgrps/bhurwitz/hurwitzlab/data/clean/mouse/doetschman/fasta"
+export SPLIT_FA_DIR="/rsgrps/bhurwitz/scottdaniel/blast-pipeline/fasta-split"
+export BLAST_OUT_DIR="/rsgrps/bhurwitz/scottdaniel/blast-pipeline/blast-out"
 export SCRIPT_DIR="$CWD/workers"
 export FA_SPLIT_FILE_SIZE=500000 # in KB
-export BLAST_CONF_FILE="/rsgrps/bhurwitz/ajacob/blast-pipeline/blastdbs"
+export BLAST_CONF_FILE="/rsgrps/bhurwitz/scottdaniel/blast-pipeline/blastdbs"
 export BLAST="/rsgrps/bhurwitz/hurwitzlab/bin/blastall"
 export JOBS=100
 
