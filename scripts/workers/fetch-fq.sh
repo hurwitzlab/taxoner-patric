b313ea3cbe8a885160a7db46ce9b94c0229783e0
Scott G. Daniel <scottdaniel25@gmail.com>
2016-03-04 15:19:02 -0700

    new files and whatnot

---
+++ scripts/workers/fetch-fq.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#PBS -W group_list=bhurwitz
+#PBS -q standard
+#PBS -l jobtype=cluster_only
+#PBS -l select=1:ncpus=2:mem=4gb
+#PBS -l place=pack:shared
+#PBS -l walltime=24:00:00
+#PBS -l cput=24:00:00
+#PBS -M scottdaniel@email.arizona.edu
+#PBS -m bea
+
+echo Host `hostname`
+
+echo Started `date`
+
+source /usr/share/Modules/init/bash
+
+TMP_FILES=$(mktemp)
+
+get_lines $FILES_TO_PROCESS $TMP_FILES $PBS_ARRAY_INDEX $STEP_SIZE
+
+NUM_FILES=$(lc $TMP_FILES)
+
+echo Found \"$NUM_FILES\" files to process
+
+export SEARCH_LIST=$(ls $CLIPPED_FASTQ)
+
+for NAME in $(cat $TMP_FILES); do 
+    echo Using $NAME
+    SHORT=$(basename $NAME '.fa')
+    echo $SHORT
+    FOUND=$(egrep $SHORT $SEARCH_LIST)
+    OUTPUT=$FILTERED_FQ/"$SHORT".filtered.fastq
+    echo Using "$NAME" as input and searching through "$FOUND" and
+    echo will output to "$OUTPUT"
+    perl $WORKER_DIR/fetch-fq.pl $NAME $FOUND $OUTPUT
+done
