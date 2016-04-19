7f7ce864d75de6ef6a0266f6769a4e3d8b3d9deb
Scott G. Daniel <scottdaniel25@gmail.com>
2016-04-19 15:58:24 -0700

    updated to include the unique_id that Taxoner uses for PATRIC

--- scripts/simpler-tax.py
+++ scripts/simpler-tax.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import argparse
 from collections import defaultdict
 
@@ -18,16 +19,27 @@ file_out1 = open(args["out1"],"w")
 
 id_to_taxa={}
 id_to_score={}
+id_to_uniq={}
+
 for line in file_in:
     line=line.rstrip('\n')
     if (':' in line):
         cols=line.split('\t')
         read_id=cols[0]
         taxa=cols[1]
+        #unique_id is the one that matches the accession number in 
+        #PATRIC_final_genome_index.txt in the patric_metadata directory
+        unique_id=cols[2]
         cigar=cols[-1]
         score=cigar.split(',')[-1].rstrip('\n').split(':')[-1]
         id_to_taxa.update({read_id:taxa})
+        id_to_uniq.update({read_id:unique_id})
         id_to_score.update({read_id:score})
 
+#This is to setup the file with some headers so we know what the hell the columns are
+if (os.stat(file_out1).st_size == 0):
+    file_out1.write('#Note: unique_id is the one that matches the accession number in PATRIC_final_genome_index.txt in the patric_metadata directory')
+    file_out1.write('Read_id\t' + 'NCBI_taxa_id\t' + 'Unique_id\t' + 'Alignment_Score\t'
+
 for read_id in id_to_taxa:
-    file_out1.write(read_id + "\t" + id_to_taxa[read_id] + "\t" + id_to_score[read_id] + "\n")
+    file_out1.write(read_id + "\t" + id_to_taxa[read_id] "\t" + id_to_uniq[read_id] + "\t" + id_to_score[read_id] + "\n")
