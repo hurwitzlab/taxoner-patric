d15aba5c5ef032da8560306d4f3dc2d7e8d1ab04
Scott G. Daniel <scottdaniel25@gmail.com>
2016-11-11 12:04:17 -0700

    added default help

--- scripts/workers/fetch-fastq.py
+++ scripts/workers/fetch-fastq.py
@@ -5,10 +5,11 @@ from Bio import SeqIO
 
 #command be like this: (from fetch-fq.sh)
 #python $WORKER_DIR/fetch-fastq.py $FASTQ $FASTA $OUTPUT
+#$FASTA can also just be a file with a list of read_ids
 
 if __name__ == "__main__":
     parser = \
-    argparse.ArgumentParser(description="Script to get fastq records using ids from a fasta file.")
+    argparse.ArgumentParser(description="Script to get fastq records using ids from a fasta file or other file with read_ids")
     parser.add_argument("-fa", "--fasta", action="store", \
         help="Fasta to used as reference ids")
     parser.add_argument("-fq", "--fastq", action="store", \
@@ -17,6 +18,10 @@ if __name__ == "__main__":
         help="Output fastq file")
     
     args = vars(parser.parse_args())
+
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
 
 output = open(args["output"],"w")
 
