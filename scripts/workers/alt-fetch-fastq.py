d1e9b08e2c22fc7940419c3372100332b91c9d0c
Scott Daniel <scottdaniel25@gmail.com>
2016-11-14 10:56:15 -0700

    think its finally working

--- scripts/workers/alt-fetch-fastq.py
+++ scripts/workers/alt-fetch-fastq.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 import os
 from Bio import SeqIO
+import errno
 #command be like this: (from fetch-fq.sh)
 #python $WORKER_DIR/fetch-fastq.py $FASTQ_DIR $FASTA $OUTPUT_DIR $MAXSCORE
 #$FASTA can also just be a file with a list of read_ids
@@ -13,9 +14,9 @@ if __name__ == "__main__":
     argparse.ArgumentParser(description="Script to get fastq records using ids from a fasta file or other file with read_ids and with a max score")
     parser.add_argument("-fa", "--fasta", action="store", \
         help="Fasta to used as reference ids")
-    parser.add_argument("-fq", "--fastq-dir", action="store", \
+    parser.add_argument("-fq", "--fastq_dir", action="store", \
         help="Dir for fastqs to search through for ids from fasta")
-    parser.add_argument("-o", "--output-dir", action="store", \
+    parser.add_argument("-o", "--output_dir", action="store", \
         help="Output dir to put fastqs in")
     parser.add_argument("-M", "--max", action="store", \
         help="Max alignment score")
@@ -26,23 +27,22 @@ if len(sys.argv)==1:
     parser.print_help()
     sys.exit(1)
 
-output_dir = args["output-dir"]
+output_dir = args["output_dir"]
 file_in = open(args["fasta"],"r")
 max_score = int(args["max"])
-fastq_dir = args["fastq-dir"]
+fastq_dir = args["fastq_dir"]
 #fqrecords=SeqIO.index(args["fastq"],"fastq")
 
 print("First get read_ids below max_score")
 
 lowqual={}
 counter1=0
-counter2=0
 
 for line in file_in:
     line=line.strip()
     cols=line.split('\t')
     read_id=cols[0]
-    score=cols[-1]
+    score=int(cols[-1])
     if (score < max_score):
         lowqual.update({read_id:score})
         counter1 += 1
@@ -55,19 +55,26 @@ fastq_files={}
 for dirpath,dirnames,files in os.walk(fastq_dir):
     for fname in files:
         if fname.endswith('clipped'):
-            fastqs[fname] = os.sep.join([dirpath,fname])
+            fastq_files[fname] = os.sep.join([dirpath,fname])
 
 print("List of files is {:s}".format(fastq_files))
 
 for fastq_name,fastq_path in fastq_files.iteritems(): #iterate through fastqs
-    shortname = fastq_name.split('.')[0]
-    output = open(os.path.join(output_dir,shortname,"lowqual.fastq"),'w')
+    shortname = fastq_name.split('.')[0] #only first "word" before period
+    if not os.path.exists(os.path.join(output_dir,shortname)): #check for dir
+        os.makedirs(os.path.join(output_dir,shortname)) #create if not there
+    output = open(os.path.join(output_dir,shortname,"lowqual.fastq"),'w') #open output file
     print("Outputting to {:s}".format(output.name))
+    fqrecords=SeqIO.index(fastq_path,"fastq")
+    counter2=0
     for hwis in lowqual.iterkeys(): #iterate through records in fasta
-        fqrecords=SeqIO.index(fastq_path,"fastq")
-        SeqIO.write(fqrecords[hwis],output,"fastq")
+        try:
+            fqrecords[hwis]
+        except KeyError as exception: #if record isn't found we keep searching
+            continue
+        SeqIO.write(fqrecords[hwis],output,"fastq") #if found we write out
         counter2 += 1
-    print("Finished writing {:d} records to {:s}".format(int(counter2),fqrecords))
+    print("Finished writing {:d} records to {:s}".format(int(counter2),fastq_name))
     fqrecords.close() #good memory citizen even though i think python has garbage collection
     output.close()
 
