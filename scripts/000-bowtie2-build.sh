bc51f8af5194b91805c96e76cecee35fbe1f3103
Scott G. Daniel <scottdaniel25@gmail.com>
2017-09-01 16:16:44 -0700

    psuedocode

--- scripts/000-bowtie2-build.sh
+++ scripts/000-bowtie2-build.sh
@@ -2,4 +2,9 @@
 
 # need a script to cat all the genomes i think
 # something like:
-#
+# find ./$GENOME_DIR -iname *.fna -print0 | xargs -I file -0 cat file > $TMP/bigun.fna
+# split $TMP/bigun > ./ into like 4GB pieces with destination being bowtie2index dir
+# make list of fna's to index in $bowtie2index_dir
+# have check for *.bt2 to see if completed (for restarting)
+# then spawn a pbs job for each *.fna to index
+# tada!
